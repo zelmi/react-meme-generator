64e709564f1994107bf04c561e2ce5de7818f88d
Zehur Elmi <zehurelmi@gmail.com>
2022-04-01 17:03:30 -0400

    useState changes images now

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,13 @@
 import memesData from "../data/memesData";
+import React, {useState} from "react";
 
 export default function Meme() {
+  let [memeImage, setMemeImage] = useState("");
   function buttonClicked() {
     let memeArray = memesData.data.memes;
     let meme = memeArray[Math.floor(Math.random() * memeArray.length)];
-    console.log(meme.name);
+    console.log(meme.url);
+    setMemeImage(meme.url);
   }
 
   return (
@@ -14,6 +17,7 @@ export default function Meme() {
         <input type="text" placeholder="Bottom text" className="form--input" />
         <button onClick={buttonClicked} className="form--button">Get a new meme image 🖼</button>
       </div>
+      <img id="memeImg" src={memeImage} alt="Flowers in Chania"/>
     </main>
   );
 }
